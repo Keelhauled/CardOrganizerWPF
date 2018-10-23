1c483c095c47e67896d9074c31b78ae0a5022891
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2018-10-23 21:56:18 +0300

    CardOrganizerKK - HScene Outfit_Load

---
+++ CardOrganizerKK/Methods_HScene.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+using static BepInEx.Logger;
+using BepInEx.Logging;
+using Illusion.Game;
+
+namespace CardOrganizerKK
+{
+    class Methods_HScene : Methods_Common
+    {
+        public override void Outfit_Load(MsgObject message)
+        {
+            Log(LogLevel.Message, $"Load outfit [{Path.GetFileName(message.path)}]");
+            Utils.Sound.Play(SystemSE.sel);
+
+            DelayAction(() =>
+            {
+                var chara = FindObjectsOfType<ChaControl>().Where((x) => x.sex == 1).ToList();
+                if(chara.Count > 0 && chara[0])
+                {
+                    chara[0].nowCoordinate.LoadFile(message.path);
+                    chara[0].Reload(false, true, true, true);
+                }
+            });
+        }
+    }
+}
