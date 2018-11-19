af59e47a8b6e69eda7125de8732809d9aaca842d
Keelhauled <keelhauledhs@gmail.com>
2018-11-20 01:01:35 +0200

    More lists to disable

--- CardOrganizerKK/DisableCharaList.cs
+++ CardOrganizerKK/DisableCharaList.cs
@@ -1,43 +1,54 @@
 ﻿using Harmony;
 using ChaCustom;
 using Studio;
-using static BepInEx.Logger;
-using BepInEx.Logging;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace CardOrganizerKK
 {
     public static class DisableCharaList
     {
+        static bool patched = false;
+        static HarmonyInstance harmony;
+        static List<MethodBase> targets = new List<MethodBase>();
+        static MethodInfo patchMethod;
+
         public static void Patch()
         {
-            var harmony = HarmonyInstance.Create("keelhauled.cardorganizerkk.disablecharalist.harmony");
-            harmony.PatchAll(typeof(DisableCharaList));
+            patched = true;
+            harmony = HarmonyInstance.Create("keelhauled.cardorganizerkk.disablecharalist.harmony");
+            patchMethod = AccessTools.Method(typeof(DisableCharaList), nameof(ReturnFalse));
+
+            DisableMethod(AccessTools.Method(typeof(CustomFileListCtrl), "AddList")); // maker chara list
+            DisableMethod(AccessTools.Method(typeof(CustomCharaFile), "Initialize"));
+            DisableMethod(AccessTools.Method(typeof(CustomCoordinateFile), "Initialize")); // maker outfit list
+            DisableMethod(AccessTools.Method(typeof(CharaList), "InitFemaleList")); // studio female list
+            DisableMethod(AccessTools.Method(typeof(CharaList), "InitMaleList")); // studio male list
+            var costumeInfoType = Assembly.GetAssembly(typeof(MPCharCtrl)).GetType("Studio.MPCharCtrl+CostumeInfo");
+            DisableMethod(AccessTools.Method(costumeInfoType, "InitFileList")); // studio outfit list
+            DisableMethod(AccessTools.Method(typeof(clothesFileControl), "Initialize")); // hscene outfit list
         }
 
-        // Prevent populating the maker character list
-        [HarmonyPrefix, HarmonyPatch(typeof(CustomFileListCtrl), "AddList")]
-        public static bool HarmonyPatch_CustomFileListCtrl_AddList()
+        public static void RemovePatches()
+        {
+            if(patched)
+            {
+                foreach(var item in targets)
+                {
+                    harmony.RemovePatch(item, patchMethod);
+                } 
+            }
+        }
+
+        static void DisableMethod(MethodBase original)
+        {
+            harmony.Patch(original, new HarmonyMethod(patchMethod), null);
+            targets.Add(original);
+        }
+
+        public static bool ReturnFalse()
         {
             return false;
-        }
-
-        [HarmonyPrefix, HarmonyPatch(typeof(CharaList), "InitFemaleList")]
-        public static bool HarmonyPatch_CharaList_InitFemaleList()
-        {
-            ShowWarning();
-            return false;
-        }
-
-        [HarmonyPrefix, HarmonyPatch(typeof(CharaList), "InitMaleList")]
-        public static bool HarmonyPatch_CharaList_InitMaleList()
-        {
-            ShowWarning();
-            return false;
-        }
-
-        public static void ShowWarning()
-        {
-            Log(LogLevel.Message, "[CardOrganizer] List disabled.");
         }
     }
 }
