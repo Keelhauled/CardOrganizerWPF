4f666c9972c8bbf194aefec5746fdefee45145ee
Keelhauled <keelhauledhs@gmail.com>
2019-01-18 18:48:08 +0200

    PH plugin start

--- CardOrganizerPH/CardOrganizerPH.cs
+++ CardOrganizerPH/CardOrganizerPH.cs
@@ -8,8 +8,40 @@ using BepInEx;
 namespace CardOrganizerPH
 {
     [BepInPlugin("keelhauled.cardorganizerph", "CardOrganizerPH", "1.0.0")]
-    class CardOrganizerPH
+    class CardOrganizerPH : BaseUnityPlugin
     {
+        public void Awake()
+        {
+            var gameobject = new GameObject(nameof(CardOrganizerPH));
+            gameobject.transform.SetParent(gameObject.transform);
+            var dispatcher = gameobject.AddComponent<UnityMainThreadDispatcher>();
 
+            var scenes = new Dictionary<string, CardHandler>
+            {
+                { "H", gameobject.AddComponent<Methods_HScene>() }
+            };
+
+            RPCClient_Plugin.Init("CardOrganizerServer", 9125, "PH", (message, id) => {
+                if(!dispatcher) Console.WriteLine("[CardOrganizer] Dispatcher dead");
+                dispatcher.Enqueue(() => scenes[id].UseCard(message));
+            });
+        }
+
+        void OnDestroy()
+        {
+            RPCClient_Plugin.StopServer();
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+            switch(SceneManager.GetActiveScene().name)
+            {
+                case "H":
+                {
+                    RPCClient_Plugin.ChangeId("HScene");
+                    break;
+                }
+            }
+        }
     }
 }
---
+++ CardOrganizerPH/Methods_HScene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PluginLibrary;
+
+namespace CardOrganizerPH
+{
+    class Methods_HScene : CardHandler
+    {
+        public override void Outfit_Load(MsgObject message)
+        {
+            
+        }
+    }
+}
