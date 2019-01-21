de454eeb2cc67e57705b4efa553eb504331068f5
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2019-01-22 01:28:53 +0200

    PH - More studio overrides

---
+++ CardOrganizerPH/Methods_Common.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Illusion;
+using PluginLibrary;
+
+namespace CardOrganizerPH
+{
+    class Methods_Common : CardHandler
+    {
+        public static void PlaySaveSound()
+        {
+
+        }
+
+        public static void PlayLoadSound()
+        {
+
+        }
+
+        public static void PlayFailSound()
+        {
+
+        }
+    }
+}
